fca1d7f401d9c3c0d1e70c4081b68837394f4330
mohk123 <mohak1802bhal@gmail.com>
2023-03-26 19:38:47 +0530

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import { Link } from "gatsby";
+import React from "react";
+import Layout from "../components/layout";
+
+export default function about() {
+  return (
+    <>
+      <Layout pageTitle="About me">
+        <p>
+          Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+        </p>
+      </Layout>
+    </>
+  );
+}
+
+export const Head = () => <title>About Me</title>;
